58cf63d22982e8caa528de1c9a26c22906d03f85
Kelvin Chan <kechan@gmail.com>
2025-08-17 23:35:35 -0400

    prov_code param

--- scripts/run_historic_metrics.py
+++ scripts/run_historic_metrics.py
@@ -4,6 +4,8 @@ import logging, sys
 from datetime import datetime
 from pathlib import Path
 
+from realestate_analytics.utils.constants import VALID_PROVINCES
+
 from realestate_analytics.etl.run_utils import load_config, get_next_job_id, update_run_csv
 from realestate_analytics.etl.historic_sold_median_metrics import SoldMedianMetricsProcessor
 from realestate_analytics.data.es import Datastore
@@ -31,6 +33,10 @@ def main():
   parser.add_argument("--es_host", help="Elasticsearch host")
   parser.add_argument("--es_port", type=int, help="Elasticsearch port")
   parser.add_argument("--force_full_load", action="store_true", help="Force a full load instead of incremental")
+  parser.add_argument("--prov_code", 
+                      default="ON", 
+                      choices=VALID_PROVINCES,
+                      help="Province code (e.g., ON, BC, AB), ON is default")
   parser.add_argument("--log_level", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], help="Logging level")
 
   #TODO: Remove this after dev
@@ -74,6 +80,7 @@ def main():
   # Log the start of the run
   logging.info("="*50)
   logging.info(f"Starting new run for job {job_id} at {datetime.now().isoformat()}")
+  logging.info(f"Province: {args.prov_code}")
   logging.info("="*50)
 
   # Initialize datastore
@@ -82,7 +89,8 @@ def main():
   # Initialize and run the processor
   processor = SoldMedianMetricsProcessor(
     job_id=job_id,
-    datastore=datastore
+    datastore=datastore,
+    prov_code=args.prov_code
   )
 
   if args.sim_failure_at_pre_transform:
